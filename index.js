f00dde24f4b823291b4ecd108c49b6df1a115ecb
torben <t.sickert[~at~]gmail.com>
2016-10-27 01:44:01 +0200

    Automatic update to new version.

---
+++ index.js
@@ -0,0 +1,99 @@
+// @flow
+// #!/usr/bin/env node
+// -*- coding: utf-8 -*-
+/** @module nginxWebNodePlugin */
+'use strict'
+/* !
+    region header
+    [Project page](http://torben.website/databaseWebNodePlugin)
+
+    Copyright Torben Sickert (info["~at~"]torben.website) 16.12.2012
+
+    License
+    -------
+
+    This library written by Torben Sickert stand under a creative commons
+    naming 3.0 unported license.
+    See http://creativecommons.org/licenses/by/3.0/deed.de
+    endregion
+*/
+// region  imports
+import {spawn as spawnChildProcess} from 'child_process'
+import Tools from 'clientnode'
+// NOTE: Only needed for debugging this file.
+try {
+    require('source-map-support/register')
+} catch (error) {}
+import type {Configuration, Services} from 'web-node/type'
+// endregion
+// region plugins/classes
+/**
+ * Launches an application server und triggers all some pluginable hooks on
+ * an event.
+ */
+export default class Nginx {
+    /**
+     * Application will be closed soon.
+     * @param services - An object with stored service instances.
+     * @param plugins - Topological sorted list of plugins.
+     * @param configuration - Mutable by plugins extended configuration object.
+     * @returns Given object of services.
+     */
+    static async exit(
+        services:Services, plugins:Array<Plugin>, configuration:Configuration
+    ):Services {
+        services.nginx.kill('SIGINT')
+        await Nginx.checkReachability(configuration, true)
+        return services
+    }
+    /**
+     * Appends an application server to the web node services.
+     * @param services - An object with stored service instances.
+     * @param configuration - Mutable by plugins extended configuration object.
+     * @returns Given and extended object of services.
+     */
+    static async preLoadService(
+        services:Services, configuration:Configuration
+    ):Services {
+        if (!services.hasOwnProperty('nginx')) {
+            services.nginx = spawnChildProcess(
+                'nginx', [], {
+                    cwd: process.cwd(),
+                    env: process.env,
+                    shell: true,
+                    stdio: 'inherit'
+                })
+            for (const closeEventName:string of Tools.closeEventNames)
+                services.nginx.on(closeEventName, Tools.getProcessCloseHandler(
+                    Tools.noop, Tools.noop, closeEventName))
+            await Nginx.checkReachability(configuration)
+        }
+        return services
+    }
+    /**
+     * Check if a nginx server is currently (not) running.
+     * @param configuration - Mutable by plugins extended configuration object.
+     * @param inverse - Boolean indicating if we should check for reachability
+     * or unreachability.
+     * @returns A promise which will be resolved if a request to given url has
+     * (not) finished. Otherwise returned promise will be rejected.
+     */
+    static async checkReachability(
+        configuration:Configuration, inverse:boolean = true
+    ):Promise<Object> {
+        const url:string = 'http' + ((
+            configuration.server.proxy.ports.length > 0 &&
+            configuration.server.proxy.ports[0] === 443
+        ) ? 's' : '') + `://` +
+        `${configuration.server.application.hostName}:` +
+        `${configuration.server.application.ports[0]}`
+        return await inverse ? Tools.checkUnreachability(
+            url, true
+        ) : Tools.checkReachability(url, true)
+    }
+}
+// endregion
+// region vim modline
+// vim: set tabstop=4 shiftwidth=4 expandtab:
+// vim: foldmethod=marker foldmarker=region,endregion:
+// endregion
---
+++ test.js
@@ -0,0 +1,46 @@
+// @flow
+// #!/usr/bin/env node
+// -*- coding: utf-8 -*-
+'use strict'
+/* !
+    region header
+    Copyright Torben Sickert (info["~at~"]torben.website) 16.12.2012
+
+    License
+    -------
+
+    This library written by Torben Sickert stand under a creative commons
+    naming 3.0 unported license.
+    See http://creativecommons.org/licenses/by/3.0/deed.de
+    endregion
+*/
+// region imports
+import * as QUnit from 'qunit-cli'
+// NOTE: Only needed for debugging this file.
+try {
+    module.require('source-map-support/register')
+} catch (error) {}
+import configuration from 'web-node/configurator.compiled'
+import type {Services} from 'web-node/type'
+
+import Index from './index'
+// endregion
+QUnit.module('index')
+QUnit.load()
+// region tests
+QUnit.test('exit', async (assert:Object):Promise<void> => {
+    let testValue:boolean = false
+    const services:Services = {nginx: {kill: ():void => {
+        testValue = true
+    }}}
+    assert.deepEqual(await Index.exit(services, [], configuration), services)
+    assert.ok(testValue)
+})
+QUnit.test('preLoadService', async (assert:Object):Promise<void> =>
+    assert.deepEqual(
+        await Index.preLoadService({nginx: {}}, configuration), {nginx: {}}))
+// endregion
+// region vim modline
+// vim: set tabstop=4 shiftwidth=4 expandtab:
+// vim: foldmethod=marker foldmarker=region,endregion:
+// endregion
