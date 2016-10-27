6f3ef007ed3bc0f976bb4024996bd14b93f5634e
torben <t.sickert[~at~]gmail.com>
2016-10-28 01:22:53 +0200

    Automatic update to new version.

--- test.js
+++ test.js
@@ -23,26 +23,26 @@ try {
 import configuration from 'web-node/configurator.compiled'
 import type {Services} from 'web-node/type'
 
-import Index from './index'
+import Nginx from './index'
 // endregion
 QUnit.module('index')
 QUnit.load()
+// TODO test rendering the tpls.
+// TODO Is only a base config with extended vars of this plugin not the
+// dependent plugins included!
+console.log('A', configuration)
 // region tests
 QUnit.test('exit', async (assert:Object):Promise<void> => {
     let testValue:boolean = false
     const services:Services = {nginx: {kill: ():void => {
         testValue = true
     }}}
-    // TODO Is only a base config with extended vars of this plugin not the
-    // dependent plugins included!
-    // TODO test rendering the tpls.
-    console.log('A', configuration)
-    assert.deepEqual(await Index.exit(services, [], configuration), services)
+    assert.deepEqual(await Ngninx.exit(services, [], configuration), services)
     assert.ok(testValue)
 })
 QUnit.test('preLoadService', async (assert:Object):Promise<void> =>
     assert.deepEqual(
-        await Index.preLoadService({nginx: {}}, configuration), {nginx: {}}))
+        await Ngninx.preLoadService({nginx: {}}, configuration), {nginx: {}}))
 // endregion
 // region vim modline
 // vim: set tabstop=4 shiftwidth=4 expandtab:
